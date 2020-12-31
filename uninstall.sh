0681d27116fd0db5a451a9bf28e23d7dd57ef35c
Mark <mark@remarkablemark.org>
2020-12-31 16:06:12 -0500

    chore: remove `sudo` from `rm` command

--- uninstall.sh
+++ uninstall.sh
@@ -15,7 +15,7 @@ echo "Uninstalling VB-Cable..."
 
 rm -rf /Library/Audio/Plugin-Ins/HAL/VBCable.Driver/
 rm -rf /Applications/VB-Audio/
-sudo rm -rf /Library/Preferences/com.vbaudio.vbcable.plist
-sudo rm -rf /Library/LaunchDaemons/com.vbaudio.vbcableagent.plist
+rm -rf /Library/Preferences/com.vbaudio.vbcable.plist
+rm -rf /Library/LaunchDaemons/com.vbaudio.vbcableagent.plist
 
 echo "VB-Cable uninstalled! Please restart your computer."
